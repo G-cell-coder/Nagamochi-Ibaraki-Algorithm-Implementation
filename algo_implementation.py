af0c4483f090a3b91fb5f25910e89e6eb32997dc
GUHAN SENTHILKUMAR <guhanece@gmail.com>
2017-06-29 23:58:25 -0700

    Update algo_implementation.py

--- algo_implementation.py
+++ algo_implementation.py
@@ -2,62 +2,62 @@
 
 import random
 
-#----------------------------CRITICAL EDGES - COMPUTING FUNCTION----------------------
+
 def critical_edges(n, l):
-    cnt = 0                                     # Counter to update on No. of Criticcal edges
+    cnt = 0                                     
     for i in range(1,n):
         for j in range(0,i):
-            if(adj[i][j]>0):                    # Check if there is a link between i & j 
-                adj[i][j]-=1                    # Delete the Link between i & j
-                localdegree = nagamochi(adj, n) # Calling algorithm to compute the connectivity
-                adj[i][j]+=1                    # Re-add the deleted link between i & j
-                if(localdegree<l):              # If current connectivity < original connectivity
-                    cnt+=1                      # Increase the critical edges
-    return cnt                                  # Return the final value of critical edge
+            if(adj[i][j]>0):                    
+                adj[i][j]-=1                  
+                localdegree = nagamochi(adj, n)
+                adj[i][j]+=1                    
+                if(localdegree<l):              
+                    cnt+=1                     
+    return cnt                                 
 
-#----------------------------NEXTNODE MA-ORDERING - COMPUTING FUNCTION-----------------
+
 def get_nextnode(nag_matrix, src_list, tot):
-    edge_adj = 0                                # Edge adjancency to src_list
+    edge_adj = 0                                
     for i in range(tot):
         if(i in src_list):
-            continue                            # If current node is in src_list ignore this loop
+            continue                           
         edge = 0
-        for j in src_list:                      # Foreach node j compute cost to src_list
+        for j in src_list:                     
             edge += nag_matrix[i][j]
-        if (edge>=edge_adj):                    # If the current number of edge is max,then it is the next node
+        if (edge>=edge_adj):                    
             edge_adj= edge
             nxt_node = i
-    return nxt_node                             # Return the next node
+    return nxt_node                             
 
-#----------------------------MA-ORDERING COMPUTING FUNCTION----------------------------
+
 def get_MAOrder(nag_matrix, tot):
-    choosenode = random.randint(0,tot-1)        # Choosing a random node
+    choosenode = random.randint(0,tot-1)        
     ma_order = [choosenode]                     
-    while(len(ma_order) <tot):                  # Form the next node by appending
+    while(len(ma_order) <tot):                 
         ma_order.append(get_nextnode(nag_matrix,ma_order,tot)) 
-    return ma_order                             # Return computed ma_ordering pattern
+    return ma_order                             
 
-#----------------------------DEGREE - COMPUTING FUNCTION-------------------------------
+-
 def get_degree(nag_matrix, n):
-    return sum(nag_matrix[n])                   # Return the degree of the selected node
+    return sum(nag_matrix[n])                   
 
 
-#----------------------------MERGING GRAPH - FUNCTION----------------------------------
+
 def merge_node(src_mat,s,t):
-    src_mat[s][t]= src_mat[t][s]= 0             # Initialize to zero the links between nodes to be merged
-    new_len = len(src_mat)-1                    # Compute length of new matrix
+    src_mat[s][t]= src_mat[t][s]= 0             
+    new_len = len(src_mat)-1                    
     tgt_mat = [[0 for i in range(new_len)] for j in range(new_len)]
     p=0
     for i in range(new_len+1):
         q=0
         flag=0
         for j in range(0,i+1):
-            if(i==max(s,t)):                    # Ignoring one of the nodes to be merged
+            if(i==max(s,t)):                   
                 flag=1
                 break
-            if(i==min(s,t)):                    # Consider the other node & sum it with ignored node
+            if(i==min(s,t)):                    
                 tgt_mat[p][q] = tgt_mat[q][p] = src_mat[min(s,t)][j]+src_mat[max(s,t)][j]
-            else:                               # Compute the links for rest of the nodes
+            else:                               
                 if(j!=s and j!=t):
                     tgt_mat[p][q] = tgt_mat[q][p] = src_mat[i][j]
                 else:
@@ -71,42 +71,41 @@ def merge_node(src_mat,s,t):
             p+=1    
     return tgt_mat
                 
-#-----------------------------IMPLEMENTATION OF NAGAMOCHI-IBARAKI ALGORITHM-----------------------
-def nagamochi(nag_matrix, tot):
-    if(tot==2):                                 # Return the degree of the last node if only 2 nodes in graph
-        return get_degree(nag_matrix,0)      
-    ma_order = get_MAOrder(nag_matrix,tot)       # First form the MA Ordering
-    last_ind = len(ma_order)-1                  # Get index of last node
-    lambda_g = get_degree(nag_matrix,ma_order[last_ind]) # Get degree of the last node
-    nag_matrix=merge_node(nag_matrix,ma_order[last_ind],ma_order[last_ind-1]) # Merge the last 2 nodes
-    return (min(lambda_g,nagamochi(nag_matrix, tot-1))) # Recursively compute connectivity till the graph contracts to 2 noded graph
 
-#------------------------------GRAPH GENERATING FUNCTION------------------------------------------
+def nagamochi(nag_matrix, tot):
+    if(tot==2):                                
+        return get_degree(nag_matrix,0)      
+    ma_order = get_MAOrder(nag_matrix,tot)      
+    last_ind = len(ma_order)-1                 
+    lambda_g = get_degree(nag_matrix,ma_order[last_ind])
+    nag_matrix=merge_node(nag_matrix,ma_order[last_ind],ma_order[last_ind-1]) 
+    return (min(lambda_g,nagamochi(nag_matrix, tot-1))) 
+
 def genearate_graph(n, maxedges):
     global adj
-    cnt = 0                                     # Counter for track of current number of edges
-    while(cnt<maxedges):                        # Check if maxedges already added
+    cnt = 0                                     
+    while(cnt<maxedges):                        
         for i in range(n):                      
             for j in range(n):
-                if (i == j):                    # Exclude self-loops
+                if (i == j):                    
                     adj[i][j] = adj[j][i] = 0
                 else:
-                    x = random.randint(0,1)     # Generate a random 0/1
-                    if (x == 1):                # If 1, add the link between i & j
+                    x = random.randint(0,1)    
+                    if (x == 1):                
                         adj[i][j] = adj[j][i] = adj[i][j]+1
-                        cnt+=x                  # Increament the number of edges in graph
-                        if (cnt >= maxedges):   # Check if max edges reached
+                        cnt+=x                  
+                        if (cnt >= maxedges):  
                             break
             if (cnt >= maxedges):
                 break
 
-n=22                                            # Set the number of nodes to 22
+n=22                                            
 print("No. OF EDGES\tDEGREE\tLAMBDA\tCRITICAL EDGES")
 print("============\t======\t======\t==============")
-for m in range(40,405,5):                       # Varies the addition of edges from 40 to 400, 405 is upperlimt which is excluded
+for m in range(40,405,5):                       
     adj=cpy=[[0 for i in range(n)] for j in range(n)]
-    di = 2*m/n                                  # Compute the average degree with the standard formulation 2* (max_edges)/(No. of Nodes)
-    generate_graph(n,m)                         # Generated a graph of 22 nodes & m edges
-    l = nagamochi(adj, n)                       # Compute the connectivity of the graph
-    criticaledges = critical_edges(n,l)                 # Compute the critical edge of the graph
-    print(str(m)+"\t\t"+str(di)+"\t"+str(l)+"\t"+str(criticaledges)) # Print the results
+    di = 2*m/n                                  
+    generate_graph(n,m)                         
+    l = nagamochi(adj, n)                       
+    criticaledges = critical_edges(n,l)                 
+    print(str(m)+"\t\t"+str(di)+"\t"+str(l)+"\t"+str(criticaledges)) 
